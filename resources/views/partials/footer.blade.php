fdfd6da1afef30f0aa4642d4d96688e8907e718c
Marco Giulietti <ga6t00n@gmail.com>
2021-01-08 09:13:44 +0100

    Footer Done - HOME Finished

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,65 +1,68 @@
-<footer class="main-footer">
-    <div class="container">
-        <div class="left-box">
-            <div class="left-nav">
-                <span class="title">Dc Comics</span>
-                <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>TV</li>
-                    <li>Games</li>
-                    <li>Videos</li>
-                    <li>News</li>
-                </ul>
-                <span class="title">Shop</span>
-                <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
-                </ul>
+<footer>
+    <section class="main-footer">
+        <div class="container">
+            <div class="left-box">
+                <div class="left-nav">
+                    <span class="title">Dc Comics</span>
+                    <ul class="mt-1 mb-1">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <span class="title">Shop</span>
+                    <ul class="mt-1 mb-1">
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="center-nav">
+                    <span class="title">DC</span>
+                    <ul class="mt-1 mb-1">
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshop</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="right-nav">
+                    <span class="title">SITES</span>
+                    <ul class="mt-1 mb-1">
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+                <p class="copy">All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="/">noted here</a>. All rights reserved. <a href="/">Cookies Settings</a></p>
             </div>
-            <div class="center-nav">
-                <span class="title">DC</span>
-                <ul>
-                    <li>Terms Of Use</li>
-                    <li>Privacy Policy (New)</li>
-                    <li>Ad Choices</li>
-                    <li>Advertising</li>
-                    <li>Jobs</li>
-                    <li>Subscriptions</li>
-                    <li>Talent Workshop</li>
-                    <li>CPSC Certificates</li>
-                    <li>Ratings</li>
-                    <li>Shop Help</li>
-                    <li>Contact Us</li>
-                </ul>
+            <div class="right-box">
+                <img src="{{ asset('images/logo-footer.png') }}" alt="">
             </div>
-            <div class="right-nav">
-                <span class="title">SITES</span>
-                <ul>
-                    <li>DC</li>
-                    <li>MAD Magazine</li>
-                    <li>DC Kids</li>
-                    <li>DC Universe</li>
-                    <li>DC Power Visa</li>
-                </ul>
-            </div>
-            <p class="copy">All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="/">noted here</a>. All rights reserved. <a href="/">Cookies Settings</a></p>
         </div>
-        <div class="right-box">
-            <img src="{{ asset('images/logo-footer.png') }}" alt="">
-        </div>
-
+    </section>
         {{-- Last BAR --}}
-        <div class="social">
+    <div class="social">
+        <div class="container">
             <a href="/" class="btn-social">Sign-up now!</a>
             <span>Follow Us</span>
             <ul class="social-list">
-                <li>F</li>
-                <li>T</li>
-                <li>Y</li>
-                <li>P</li>
-                <li>L</li>
+                <li><i class="fab fa-facebook-f"></i></li>
+                <li><i class="fab fa-twitter"></i></li>
+                <li><i class="fab fa-youtube"></i></li>
+                <li><i class="fab fa-pinterest-p"></i></li>
+                <li><i class="fas fa-map-marker-alt"></i></li>
             </ul>
         </div>
     </div>

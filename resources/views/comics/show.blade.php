8dc135a64cc392ca5cf5ddf9a92e86fbb4c7d71e
Marco Giulietti <ga6t00n@gmail.com>
2021-01-08 13:51:43 +0100

    Detailed Page Done

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,7 +4,7 @@
     <main class="wrap-home">
         
         {{-- Hero Box --}}
-        <section class="general-hero" style="background-image: url({{ $comic['image-hero'] }});">
+        <section class="general-hero bb-blue" style="background-image: url({{ $comic['image-hero'] }});">
             <div class="container">
                 <img src="{{ $comic['image-cover'] }}" alt="{{ $comic['title'] }}">
             </div>
@@ -13,9 +13,33 @@
         {{-- Comic Detail --}}
         <section class="details">
             <div class="container">
-                <h1>{{ $comic['title'] }}</h1>
-                <div class="price">{{ $comic['price'] }}</div>
-                <div class="text">{!! $comic['body'] !!}</div>
+                <div class="detail-sx">
+                    <h1>{{ $comic['title'] }}</h1>
+                    <div class="green-box mb-3">
+                        <div class="price-holder">
+                            <span class="price">U.S. Price {{ $comic['price'] }}</span>
+                            <span>Available on 11/10</span>    
+                        </div>
+                        <div class="check-av">
+                            Check Availability
+                        </div>
+                    </div>
+                    <div class="text">
+                        {!! $comic['body'] !!}
+                    </div>
+
+                </div>
+                <div class="adv">
+                    <h3>Advertisement</h3>
+                    <img src="{{ asset('images/adv.png') }}" alt="">
+                </div>
+            </div>
+        </section>
+
+        {{-- Comic Info --}}
+        <section class="comic-info">
+            <div class="container">
+
             </div>
         </section>
     </main>
